bf032e21de80108e8a8259249d9a8e586a5a3aed
Gabriel De Souza Passos <44300759+EastBeng@users.noreply.github.com>
2020-11-25 14:21:07 -0300

    Create Agenda.cs

---
+++ Mini E-commerce/Agenda.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+class Agenda{
+  public List<Cliente> listaCli;
+
+  //construtor com inicialização da lista de usu
+  public Agenda(){
+    listaCli = new List<Cliente>();
+  }
+}
