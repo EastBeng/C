4420bc969258fd7866de4406f8a02aa7c417807f
Gabriel De Souza Passos <44300759+EastBeng@users.noreply.github.com>
2020-11-25 14:28:12 -0300

    Create carros.cs

---
+++ Lista de Carros/carros.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Carros{
+
+public List<string> carro;
+public List<double> consumo;
+
+public Carros(){
+  carro = new List<string>();
+  consumo = new List<double>();
+}
+
+
+}
