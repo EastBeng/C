da9c78d5d3f7e240720704ebb92028baf3f4af19
Gabriel De Souza Passos <44300759+EastBeng@users.noreply.github.com>
2020-11-25 14:52:27 -0300

    Add files via upload

---
+++ Projeto GymAtHome/exercicios.cs
@@ -0,0 +1,128 @@
+using System;
+
+class Exercicios{
+
+public static void Explica(){
+  Console.WriteLine("Pronto/a para começar?!");
+  Console.WriteLine("Serão 2 sessões com 8 Exercicios(na segunda sessão serão os mesmos exercicios da primeira).\n(Obs: Caso não entenda como se faz o exercicio, clique no link colocado depois da explicação do exercicio para ver uma imagem de como funciona.\n");
+}
+
+public static void ExerciciosSuperiorMasculino(){
+  
+  for (int i = 0; i < 2; i++){
+    Console.WriteLine("******* Exercicios Para Parte Superior do Corpo *******\n");
+
+    Console.WriteLine("Exercício 1 - 30 Polichinelos.\n");
+    Console.WriteLine("Como fazer >> Inicialmente fique com as pernas totalmente \nfechadas e com as mãos coladas nas coxas, depois, com saltos no mesmo \nlugar e com movimentos sincronizados de braços e pernas, seguindo os \nseguintes passos: com um pulo, abre-se as pernas, levanta-se os braços acima da cabeça ao máximo tocando uma mão na outra e abaixa-os fechando \nas pernas e tocando as mãos novamente nas coxas, e repita fazendo 30 vezes. || LINK > https://guiatreinofuncional.com/wp-content/uploads/2019/08/polichinelo-exerc%C3%ADcios-em-casa-1024x753.jpg\n");
+
+    Console.WriteLine("Exercício 2 - 35 Levantamento Lateral dos Braços Sem Peso.\n");
+    Console.WriteLine("Como fazer >> Fique de pé com os pés alinhados com os ombros. Levante \nseus braços na lateral até a altura dos ombros e, em seguida baixe-os. \nRepita o exericios 35 vezes. || LINK > https://www.hipertrofia.org/blog/wp-content/uploads/2017/09/execu%C3%A7%C3%A3o-eleva%C3%A7%C3%A3o-lateral-para-ombros.jpg\n");
+
+    Console.Write("Para continuar clique a tecla Enter ou digite 'S'.");
+    string enter = Console.ReadLine();
+
+    if (enter == "" || enter == "s" || enter == "S"){
+      
+      Console.Clear();
+      Console.WriteLine("******* Exercicios Para Parte Superior do Corpo *******\n");
+
+      Console.WriteLine("Exercício 3 - 70 Socos (35 cada lado).\n");
+      Console.WriteLine("Como fazer >> Fique de pé, com uma das pernas para frente e os joelhos \nlevemente dobrados. Depois feche os punhos e de socos ao vento. || LINK > https://image.freepik.com/fotos-gratis/treino-de-boxe-jovem-conceito-atletico-do-encaixotamento-do-exercicio-do-pugilista-boxer-soco-mao-por-saco-de-pancadas_152404-4329.jpg\n");
+
+      Console.WriteLine("Exercício 4 - 10 à 20 Flexões (Se não conseguir fazer 10 tente fazer de 3 à 6 dando uma pausa em cada uma).\n");
+      Console.WriteLine("Como fazer >> Levantando e abaixando o corpo com os braços, matenha o \ncorpo reto, este exercicio trabalha os músculos do peito, ombros, \ntríceps, costas e pernas. || LINK > https://2.bp.blogspot.com/-WRMumXDSH80/WZN4MnqeA2I/AAAAAAACCIQ/MgUZdWw09gMBNdH4oQjRILRrxRUG16opACK4BGAYYCw/s1600/como%2Bfazer%2Bcorretamente%2Bflexa%25CC%2583o.jpg\n");
+
+      Console.WriteLine("Exercício 5 - 30 Abdominais(Se não conseguir fazer todas de uma só vez, faça 15 inicialmente e depois mais 15 ).\n");
+      Console.WriteLine("Deite de costas com os joelhos dobrados e os braços atrás das orelhas. \nLevate a parte superior do corpo tirando-a do chão. Mantenha-a erguida \npor alguns segundos e retorne lentamente. || LINK > https://lh3.googleusercontent.com/vjJdu_IUSU9wOH70XQ1NciynsLFDI5xz_P3EPuzNe5IikhUdzGpqZiUeQwzC6VEDuAv-bTpgQDuEwm2ML_laGt1cZhfmpQWXN3ODP90nSA5dMUTHAlqsgnb-pdbDkz_cWMghwn8a\n");
+      
+
+      Console.Write("Para continuar clique a tecla Enter ou digite 'S'.");
+      enter = Console.ReadLine();
+
+    if (enter == "" || enter == "s" || enter == "S"){
+      Console.Clear();
+      Console.WriteLine("******* Exercicios Para Parte Superior do Corpo *******\n");
+      
+      Console.WriteLine("Exercicio 6 - 20 Abdominais Cruzados(Se não conseguir fazer todas de uma só vez, faça 10 inicialmente e depois mais 10 ).\n");
+      Console.WriteLine("Como fazer >> Deite-se de costas com seus joelhos dobrados e com suas \nmãos atrás das orelhas. Levante e gire seu tronco de modo que seu \ncotovelo direito mova-se na direção de seu joelho esquerdo. Repita com o outro lado.|| LINK > https://revistashape.com.br/wp-content/uploads/2018/08/bicicleta-no-ar-como-fazer.gif\n");
+
+      Console.WriteLine("Exercicio 7 - 15 à 30 segundos Prancha.\n");
+      Console.WriteLine("Como fazer >> Mantenha a posição de flexão com o peso do corpo \nsustentado pelos antebraços cotovelos e dedos dos pés. || LINK > https://s3.amazonaws.com/dinder.com.br/wp-content/uploads/sites/419/2019/10/plank.gif\n");
+
+      Console.WriteLine("Exercicio 8 - 30 segundos Alongamento Cobra.\n");
+      Console.WriteLine("Como fazer >>>> Deite-se de bruços e dobre seus cotovelos com as mãos debaixo dos ombros. Empurre o peito afastando-o máximo possivel do chão. || LINK > https://i.ytimg.com/vi/5rlSICnMTBs/maxresdefault.jpg\n");
+      
+        if (i < 1){
+         //Console.WriteLine("Proxima Sessão - Descanse 30 segundos.\n");
+          }
+        }
+    }
+  }
+  Console.WriteLine("Treino Finalizado! Você se saiu muito bem.");
+}
+
+public static void ExerciciosInferiorMasculino(){
+  Console.WriteLine("******* Exercicios Para Parte Inferior do Corpo *******");
+
+}
+
+public static void ExerciciosSuperiorFeminino(){
+  
+ //for (int i = 0; i < 2; i++){
+    Console.WriteLine("******* Exercicios Para Parte Superior do Corpo *******\n");
+
+    Console.WriteLine("Exercício 1 - 30 Polichinelos.\n");
+    Console.WriteLine("Como fazer >> Inicialmente fique com as pernas totalmente \nfechadas e com as mãos coladas nas coxas, depois, com saltos no mesmo \nlugar e com movimentos sincronizados de braços e pernas, seguindo os \nseguintes passos: com um pulo, abre-se as pernas, levanta-se os braços acima da cabeça ao máximo tocando uma mão na outra e abaixa-os fechando \nas pernas e tocando as mãos novamente nas coxas, e repita fazendo 30 vezes. || LINK > https://emagrecarapidoportalsaude.com/wp-content/uploads/2020/02/polichinelo-GIF-1.gif\n");
+
+    
+    Console.WriteLine("Exercício 2 - 12 Abdominais em V.\n");
+    Console.WriteLine("Como fazer >> Deve-se deitar com a barriga para cima e levantar um pouco as pernas e o tronco, colocando os braços para frente, realizando esses dois movimentos ao mesmo tempo, contraindo bem a musculatura abdominal e com cuidado para não forçar o pescoço. || LINK > https://vida-saudavel.hi7.co/vida-saudavel/vida-saudavel-570b1806151b1.jpg\n");
+
+    Console.Write("Para continuar clique a tecla Enter ou digite 'S'.");
+    string enter = Console.ReadLine();
+
+    if (enter == "" || enter == "s" || enter == "S"){
+      Console.Clear();
+      Console.WriteLine("******* Exercicios Para Parte Superior do Corpo *******\n");
+
+      Console.WriteLine("Exercício 3 - 12 Elevação de Pernas.\n");
+      Console.WriteLine("Como fazer >> Deite-se de costas e coloque as mãos por baixo das ancas \ncomo apoio. Em seguida, levante as pernas até formar um ângulo reto\n(90 graus) com o chão. Baixe as pernas lentamente e repita o exercicio. || LINK > https://www.mundoboaforma.com.br/wp-content/uploads/2016/10/elevacao-de-pernas-e1475519816360.jpg\n");
+
+      
+      Console.WriteLine("Exercício 4 - 20 Toques de Calcanhar (10 cada lado).\n");
+      Console.WriteLine("Como fazer >> Deite-se no chão com suas pernas dobradas e seus braços ao lado do corpo. Levante um pouco a parte superior do corpo distanciando-a do chão e faça com que suas mãos toquem nos calcanhares alternadamente. || LINK > https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/Fitness-Exercise/1679-Adonis_Belt_400x400-Lateral_Heel_Taps.gif?h=840\n");
+
+      Console.WriteLine("Exercício 5 - 30 Abdominais(Se não conseguir fazer todas de uma só vez, faça 15 inicialmente e depois mais 15 ).\n");
+      Console.WriteLine("Deite de costas com os joelhos dobrados e os braços atrás das orelhas. \nLevate a parte superior do corpo tirando-a do chão. Mantenha-a erguida \npor alguns segundos e retorne lentamente. || LINK > https://static1.semprepronta.com/articles/7/63/57/@/53512-nao-da-pra-esquecer-das-classicas-ne-na-article_image-1.gif\n");
+
+
+    Console.Write("Para continuar clique a tecla Enter ou digite 'S'.");
+    enter = Console.ReadLine();
+
+    if (enter == "" || enter == "s" || enter == "S"){
+      Console.Clear();
+
+      Console.WriteLine("Exercicio 6 - 20 Abdominais Cruzados(Se não conseguir fazer todas de uma só vez, faça 10 inicialmente e depois mais 10 ).\n");
+      Console.WriteLine("Como fazer >> Deite-se de costas com seus joelhos dobrados e com suas \nmãos atrás das orelhas. Levante e gire seu tronco de modo que seu \ncotovelo direito mova-se na direção de seu joelho esquerdo. Repita com o outro lado.|| LINK > https://revistashape.com.br/wp-content/uploads/2018/08/bicicleta-no-ar-como-fazer.gif\n");
+
+      Console.WriteLine("Exercicio 7 - 15 à 30 segundos Prancha.\n");
+      Console.WriteLine("Como fazer >> Mantenha a posição de flexão com o peso do corpo sustentado pelos \nantebraços cotovelos e dedos dos pés. || LINK > https://recemcasados.com.br/wp-content/uploads/2018/11/prancha-simples.gif\n");
+
+      Console.WriteLine("Exercicio 8 - 30 segundos Alongamento Cobra.\n");
+      Console.WriteLine("Como fazer >> Deite-se de bruços e dobre seus cotovelos com as mãos debaixo dos ombros. Empurre o peito afastando-o máximo possivel do chão. || LINK > https://i.ytimg.com/vi/JDcdhTuycOI/maxresdefault.jpg\n");
+      
+      //if (i < 1){
+       // Console.WriteLine("Proxima Sessão - Descanse 30 segundos.");
+      //}
+      }
+    }
+ // }
+  Console.WriteLine("Treino Finalizado! Você se saiu muito bem.");
+}
+
+public static void ExerciciosInferiorFeminino(){
+  Console.WriteLine("******* Exercicios Para Parte Inferior do Corpo *******");
+
+}
+
+}
---
+++ Projeto GymAtHome/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+  Apresentacao apresentaInstancia = new Apresentacao();
+  
+  Apresentacao.Abertura(); //obs 
+  apresentaInstancia.Sexo();
+
+
+  }
+}
